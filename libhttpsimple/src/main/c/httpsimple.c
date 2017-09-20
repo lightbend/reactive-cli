0049882f8091d78fd70baa3cf552b4e9a6dbf83f
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-09-20 16:51:21 +1000

    Add copyright header

--- libhttpsimple/src/main/c/httpsimple.c
+++ libhttpsimple/src/main/c/httpsimple.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2014-2016 Lightbend, Inc. All rights reserved.
+ * No information contained herein may be reproduced or transmitted in any form
+ * or by any means without the express written permission of Lightbend, Inc.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- libhttpsimple/src/main/c/httpsimple.h
+++ libhttpsimple/src/main/c/httpsimple.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2014-2016 Lightbend, Inc. All rights reserved.
+ * No information contained herein may be reproduced or transmitted in any form
+ * or by any means without the express written permission of Lightbend, Inc.
+ */
+
 #ifndef httpsimple_h__
 #define httpsimple_h__
 
